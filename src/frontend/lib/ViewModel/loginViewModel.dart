3ec2d1cf38469b30213a8578b551c2c510b7320e
Artur Quirino <arturquirino6@gmail.com>
2018-03-31 12:10:27 -0300

    creation of folders to separate the view from the viewmodel and login mock

---
+++ src/frontend/lib/ViewModel/loginViewModel.dart
@@ -0,0 +1,9 @@
+class LoginViewModel {
+
+  bool logIn(String email, String password){
+    if(email == "aaa" && password == "111")
+      return true;
+    
+    return false;
+  }
+}
--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login.dart';
+import 'View/loginView.dart';
 
 void main() => runApp(new MyApp());
 
