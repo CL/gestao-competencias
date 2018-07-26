b492f3e8ad0a5d95a3ff2065c7dc27702f406755
Vitor Rodarte <vitor.ricoy@mrv.com.br>
2018-07-26 11:12:30 -0300

    Fixed port to 0.0.0.0

--- src/backend/app.py
+++ src/backend/app.py
@@ -9,4 +9,4 @@ app.register_blueprint(login, url_prefix='/login')
 app.register_blueprint(skills, url_prefix='/skills')
 app.register_blueprint(search, url_prefix='/search')
 
-app.run()
+app.run(host='0.0.0.0')
