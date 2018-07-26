5c081b3cda5ee792e3e0906f39feb57cf99754f0
Vitor Rodarte <vitor.ricoy@mrv.com.br>
2018-07-26 11:19:47 -0300

    Fix flask

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from Host.LoginController import login
 from Host.SkillsController import skills
@@ -9,4 +11,4 @@ app.register_blueprint(login, url_prefix='/login')
 app.register_blueprint(skills, url_prefix='/skills')
 app.register_blueprint(search, url_prefix='/search')
 
-app.run(host='0.0.0.0')
+app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))
