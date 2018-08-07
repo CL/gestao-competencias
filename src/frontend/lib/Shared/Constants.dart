6bd5ec96aec3936dc5b5263e6b3efbe8e3e30832
Vitor Rodarte <vitor.ricoy@mrv.com.br>
2018-08-07 08:24:07 -0300

    Remoção das variaveis prd

--- src/frontend/lib/Shared/Constants.dart
+++ src/frontend/lib/Shared/Constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String URL_BACKEND = "https://cc656a5f.ngrok.io/";
+  static const String URL_BACKEND = "https://dry-temple-65102.herokuapp.com/";
   static const String PATH_LOGIN = "login";
   static const String PATH_SKILLS = "skills";
   static const String PATH_SEARCH = "search";
